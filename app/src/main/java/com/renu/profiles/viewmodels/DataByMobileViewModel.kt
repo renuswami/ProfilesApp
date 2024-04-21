50f537f4b931d2c4df27626dc8a01b9e56f10079
renuswami <renuswami2001@gmail.com>
2024-04-21 12:10:14 +0530

    initial commit!!

--- app/src/main/java/com/renu/profiles/viewmodels/DataByMobileViewModel.kt
+++ app/src/main/java/com/renu/profiles/viewmodels/DataByMobileViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.renu.profiles.profileRepository.ProfileRepo
 import com.renu.profiles.profiledata.ProfileData
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 
@@ -13,14 +14,16 @@ class DataByMobileViewModel : ViewModel() {
 
     var profileData = mutableStateOf(ProfileData())
     val dataByMobile = mutableStateOf( mutableListOf(ProfileData()))
+    val isEmpty = MutableStateFlow(false)
 
     private val profileRepo = ProfileRepo()
 
     val mobile = mutableStateOf("")
     fun getDataByMobile() {
-
+        isEmpty.value = true
     viewModelScope.launch {
         dataByMobile.value = profileRepo.getDataByMobile(profileData.value.mobile)
+        isEmpty.value = false
         Log.e("GETDATABYMOBILE", dataByMobile.value.toString())
     }
 }
