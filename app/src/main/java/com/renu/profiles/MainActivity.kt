f3c6a52f16739b8bd71ece691e8c034fe60abbbf
renuswami <renuswami2001@gmail.com>
2024-04-25 22:06:08 +0530

    Updated

--- app/src/main/java/com/renu/profiles/MainActivity.kt
+++ app/src/main/java/com/renu/profiles/MainActivity.kt
@@ -1,48 +1,40 @@
 package com.renu.profiles
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Start
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.renu.profiles.ui.theme.ProfilesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val profileViewModel = ProfileViewModel()
             ProfilesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column (
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ){
-                        Button(onClick = {
-                            val navigate = Intent(this@MainActivity, ProfileFormActivity::class.java)
-                            startActivity(navigate)
-                         }) {
-                            Text(text = "Profile Form", fontSize = 18.sp)
-                        }
+                    val nav = rememberNavController()
 
-                        Button(onClick = {
-                            val navigate = Intent(this@MainActivity, ProfileInfoActivity::class.java)
-                            startActivity(navigate)
-                        }) {
-                            Text(text = "Profile Info", fontSize = 18.sp)
+                    NavHost(navController = nav, startDestination = "Start"){
+                        composable("Start"){
+                            ProfileForm(profileViewModel =profileViewModel, nav =nav)
+                        }
+                        composable("ProfileInfo"){
+                            ProfileInfo(profileViewModel = profileViewModel, nav = nav)
                         }
 
                     }
@@ -51,5 +43,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 
