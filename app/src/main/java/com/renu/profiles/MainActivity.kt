975f30a520ba00d81490b61ffdd7e49af3181a64
renuswami <renuswami2001@gmail.com>
2024-04-25 21:34:10 +0530

    Trial composable edit

--- app/src/main/java/com/renu/profiles/MainActivity.kt
+++ app/src/main/java/com/renu/profiles/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
@@ -52,3 +53,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun Trial() {
+    Text(text = "Renu")
+}
