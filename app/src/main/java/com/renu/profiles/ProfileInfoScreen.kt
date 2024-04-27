b8a575bdd1b4e85e22ae7e8f304dc5e0b6f49b38
renuswami <renuswami2001@gmail.com>
2024-04-27 12:16:14 +0530

    profileInfo information function working

--- app/src/main/java/com/renu/profiles/ProfileInfoScreen.kt
+++ app/src/main/java/com/renu/profiles/ProfileInfoScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.navigation.NavHostController
 
+
 @Composable
 fun ProfileInfo(profileViewModel: ProfileViewModel, nav: NavHostController) {
 
